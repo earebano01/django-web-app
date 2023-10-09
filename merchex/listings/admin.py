eff7ad2347bcf6ae119193d321378675703f6e79
Adonis Rebano <earebano01@monccnb.ca>
2023-10-09 12:18:57 -0300

    partie 2 openclassrooms

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band, Listing
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing)
